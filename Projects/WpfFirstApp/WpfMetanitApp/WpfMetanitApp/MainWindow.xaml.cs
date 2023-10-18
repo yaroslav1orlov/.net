1466b464b826da6c55f5d381034848f0314e38d2
Yaroslav Orlov <yaroslav1orlov@gmail.com>
2023-10-18 11:48:54 +0300

    Added a Metanit progekt Chopter 1-2

---
+++ Projects/WpfFirstApp/WpfMetanitApp/WpfMetanitApp/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace XamlApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = textBox1.Text;
+            if (text != "")
+            {
+                MessageBox.Show(text);
+            }
+        }
+    }
+}
